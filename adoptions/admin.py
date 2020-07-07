53b17b4a99e930a61443d6587d062cefa8815508
Katy Deng <katydeng@styleseat.com>
2020-07-07 15:32:38 -0700

    completed

--- adoptions/admin.py
+++ adoptions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Pet    # equal to import Pet from "./models"
+# pd: learning123  for this app's djangosuppersure
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'species', 'breed', 'age', 'sex']
